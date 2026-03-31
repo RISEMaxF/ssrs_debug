2186d26bb1de9f41164036bd2b38d22d9a345599
Max Fransson <max.fransson@ri.se>
2026-03-31 16:51:57 +0200

    Override entrypoint for zenoh scripts

--- list_location_topics.sh
+++ list_location_topics.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Show all location_fix topics being published on Zenoh (any entity)
-docker run --rm --network host ghcr.io/rise-maritime/keelson-connector-blueos:v0.2.0 python3 -c "
+docker run --rm --network host --entrypoint python3 ghcr.io/rise-maritime/keelson-connector-blueos:v0.2.0 -c "
 import zenoh
 s = zenoh.open(zenoh.Config())
 def cb(sample):
